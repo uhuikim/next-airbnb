662000e4d9fdcc65b1e7ca2d4c9693b7d387fa14
uhuikim <uhui94@naver.com>
2023-06-02 17:15:36 +0900

    feat: 등록한 숙소 리스트 메인페이지에 표출

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,32 @@
-import { Toaster } from "react-hot-toast";
-import RegisterModal from "./components/modals/RegisterModal";
-import LoginModal from "./components/modals/LoginModal";
+import { Toaster } from 'react-hot-toast';
+import RegisterModal from './components/modals/RegisterModal';
+import LoginModal from './components/modals/LoginModal';
 
-import Navbar from "./components/navbar/Navbar";
-import "./globals.css";
-import { Nunito } from "next/font/google";
-import getCurrentUser from "./actions/getCurrentUser";
-import RentModal from "./components/modals/RentModal";
+import Navbar from './components/navbar/Navbar';
+import './globals.css';
+import { Nunito } from 'next/font/google';
+import getCurrentUser from './actions/getCurrentUser';
+import RentModal from './components/modals/RentModal';
 
-const font = Nunito({ subsets: ["latin"] });
+const font = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Airbnb",
-  description: "Airbnb clone",
+    title: 'Airbnb',
+    description: 'Airbnb clone'
 };
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const currentUser = await getCurrentUser();
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <RegisterModal />
-        <LoginModal />
-        <RentModal />
-        <Navbar currentUser={currentUser} />
-        <Toaster />
-        {children}
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+    const currentUser = await getCurrentUser();
+    return (
+        <html lang="en">
+            <body className={font.className}>
+                <RegisterModal />
+                <LoginModal />
+                <RentModal />
+                <Navbar currentUser={currentUser} />
+                <Toaster />
+                <div className="pb-20 pt-28"> {children}</div>
+            </body>
+        </html>
+    );
 }

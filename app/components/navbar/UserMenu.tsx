98e40173c024b423c45bf53c6ae19ef1491193da
uhuikim <uhui94@naver.com>
2023-06-08 16:44:39 +0900

    feat : trip 페이지 마크업

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,83 +1,94 @@
-"use client";
-import { AiOutlineMenu } from "react-icons/ai";
-import Avatar from "../Avatar";
-import { useCallback, useState } from "react";
-import MenuItem from "./MenuItem";
+'use client';
+import { AiOutlineMenu } from 'react-icons/ai';
+import Avatar from '../Avatar';
+import { useCallback, useState } from 'react';
+import MenuItem from './MenuItem';
 
-import useRegisterModal from "@/app/hooks/useRegisterModal";
-import useLoginModal from "@/app/hooks/useLoginModal";
-import useRentModal from "@/app/hooks/useRentModal";
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+import useRentModal from '@/app/hooks/useRentModal';
 
-import { User } from "@prisma/client";
-import { signOut } from "next-auth/react";
+import { User } from '@prisma/client';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps {
-  currentUser?: User | null;
+    currentUser?: User | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
-  const registerModal = useRegisterModal();
-  const loginModal = useLoginModal();
-  const rentModal = useRentModal();
+    const router = useRouter();
+    const registerModal = useRegisterModal();
+    const loginModal = useLoginModal();
+    const rentModal = useRentModal();
 
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = useCallback(() => {
-    setIsOpen((value) => !value);
-  }, []);
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
 
-  const onRent = useCallback(() => {
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
+    const onRent = useCallback(() => {
+        if (!currentUser) {
+            return loginModal.onOpen();
+        }
 
-    rentModal.onOpen();
-  }, [currentUser, loginModal, rentModal]);
+        rentModal.onOpen();
+    }, [currentUser, loginModal, rentModal]);
 
-  return (
-    <div className="relative">
-      <div className="flex flex-row items-center gap-3">
-        <div
-          onClick={onRent}
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-netural-100 transition cursor-pointer"
-        >
-          Airbnb your home
-        </div>
-        <div
-          onClick={toggleOpen}
-          className="p-4 md:py-1 md:px-2 border-[1px] border-netural-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
-        >
-          <AiOutlineMenu />
-          <div className="hidden md:block">
-            <Avatar src={currentUser?.image} />
-          </div>
-        </div>
-      </div>
+    return (
+        <div className="relative">
+            <div className="flex flex-row items-center gap-3">
+                <div
+                    onClick={onRent}
+                    className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-netural-100 transition cursor-pointer"
+                >
+                    Airbnb your home
+                </div>
+                <div
+                    onClick={toggleOpen}
+                    className="p-4 md:py-1 md:px-2 border-[1px] border-netural-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                >
+                    <AiOutlineMenu />
+                    <div className="hidden md:block">
+                        <Avatar src={currentUser?.image} />
+                    </div>
+                </div>
+            </div>
 
-      {isOpen && (
-        <div className="absolute rounded-xl shadow-md w-[40vw] bg-white overflow-hidden right-0 top-12 text-sm">
-          <div className="flex flex-col cursor-pointer">
-            {currentUser ? (
-              <>
-                <MenuItem onClick={() => {}} label="My trips" />
-                <MenuItem onClick={() => {}} label="My favorites" />
-                <MenuItem onClick={() => {}} label="My reservations" />
-                <MenuItem onClick={() => {}} label="My properties" />
-                <MenuItem onClick={rentModal.onOpen} label="Airbnb my home" />
-                <hr />
-                <MenuItem onClick={() => signOut()} label="Logout" />
-              </>
-            ) : (
-              <>
-                <MenuItem onClick={loginModal.onOpen} label="Login" />
-                <MenuItem onClick={registerModal.onOpen} label="Sing up" />
-              </>
+            {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[40vw] bg-white overflow-hidden right-0 top-12 text-sm">
+                    <div className="flex flex-col cursor-pointer">
+                        {currentUser ? (
+                            <>
+                                <MenuItem
+                                    onClick={() => router.push('/trips')}
+                                    label="My trips"
+                                />
+                                <MenuItem onClick={() => {}} label="My favorites" />
+                                <MenuItem onClick={() => {}} label="My reservations" />
+                                <MenuItem onClick={() => {}} label="My properties" />
+                                <MenuItem
+                                    onClick={rentModal.onOpen}
+                                    label="Airbnb my home"
+                                />
+                                <hr />
+                                <MenuItem onClick={() => signOut()} label="Logout" />
+                            </>
+                        ) : (
+                            <>
+                                <MenuItem onClick={loginModal.onOpen} label="Login" />
+                                <MenuItem
+                                    onClick={registerModal.onOpen}
+                                    label="Sing up"
+                                />
+                            </>
+                        )}
+                    </div>
+                </div>
             )}
-          </div>
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default UserMenu;

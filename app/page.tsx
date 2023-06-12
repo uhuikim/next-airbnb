5cd7187f0b897107caee76e268df07d188250eb7
uhuikim <uhui94@naver.com>
2023-06-12 16:44:47 +0900

    feat: loading & error 마크업

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ const Home = async ({ searchParams }: HomeProps) => {
     if (listings.length === 0) {
         return <EmptyState showReset />;
     }
+
     return (
         <Container>
             <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
